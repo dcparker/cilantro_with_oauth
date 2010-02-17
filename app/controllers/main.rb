224e9a6c146a3feb3f0bbf8d927a9f82bb05c4dc
dcparker <dcparker@gmail.com>
2010-02-17 18:18:38 -0500

    Modified to work with cilantro changes in named routes.

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -1,18 +1,18 @@
 class Main < Application
   namespace '/'
 
-  get :home do
+  get '' => :home do
     view.session = session
     view :index
   end
 
-  get :oauth_logout => 'logout' do
+  get 'logout' => :oauth_logout do
     session[:access_token_token] = nil
     session[:access_token_secret] = nil
     redirect url(:home)
   end
 
-  get :oauth_login => 'login' do
+  get 'login' => :oauth_login do
     begin
       request_token = Cilantro.config[:OAuthConsumer].get_request_token
       session[:request_token_token] = request_token.token
@@ -24,7 +24,7 @@ class Main < Application
     end
   end
 
-  get :callback_url => 'callback_url' do
+  get 'callback_url' => :callback_url do
     begin
       request_token = OAuth::RequestToken.new(Cilantro.config[:OAuthConsumer], session[:request_token_token], session[:request_token_secret])
       access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
@@ -39,7 +39,15 @@ class Main < Application
     end
   end
 
-  get :blog => 'blogs/mine' do
+  get(/remix_api\/(.*)/ => :remix) do |rw_api|
+    if session[:access_token_token] && session[:access_token_secret]
+      # ...
+    else
+      redirect url(:home)
+    end
+  end
+
+  get 'blogs/mine' => :blog do
     if session[:access_token_token] && session[:access_token_secret]
       access_token = OAuth::AccessToken.new(Cilantro.config[:OAuthConsumer], session[:access_token_token], session[:access_token_secret])
       blog_posts = access_token.get("/api/v1/blog_posts.json").body
